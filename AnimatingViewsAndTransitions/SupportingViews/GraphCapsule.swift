25b33a3ad63cb5ef8198c9fdc006a33660cc02b3
freak_lather <ajaysinghlather@gmail.com>
2020-04-09 18:09:53 +0800

    initial commit

---
+++ AnimatingViewsAndTransitions/SupportingViews/GraphCapsule.swift
@@ -0,0 +1,21 @@
+//
+//  GraphCapsule.swift
+//  AnimatingViewsAndTransitions
+//
+//  Created by ajay lather on 09/04/20.
+//  Copyright © 2020 Ajay Lather. All rights reserved.
+//
+
+import SwiftUI
+
+struct GraphCapsule: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GraphCapsule_Previews: PreviewProvider {
+    static var previews: some View {
+        GraphCapsule()
+    }
+}
--- AnimatingViewsAndTransitions/SupportingViews/HikeGraph.swift
+++ AnimatingViewsAndTransitions/SupportingViews/HikeGraph.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HikeGraph.swift
 //  AnimatingViewsAndTransitions
 //
 //  Created by ajay lather on 09/04/20.
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HikeGraph: View {
     var body: some View {
-        Text("Hello, World!")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HikeGraph_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HikeGraph()
     }
 }
